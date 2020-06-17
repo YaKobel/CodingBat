b5ec44407dc0c17d990ff5fdec08beb30ce6ebdf
Yaroslav Kobel <yarokobel@gmail.com>
2020-06-17 21:46:07 +0300

    finish string 1

--- CodingBat/String-1.py
+++ CodingBat/String-1.py
@@ -21,6 +21,9 @@
 # def make_abba(a, b):
 #   return (a + b) + (b + a)
 
+# def make_abba(a, b):
+#   return a + b + b + a
+
 
 
 # 3. Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g. "<<word>>".
@@ -33,3 +36,107 @@
 #   hash_tag = "<{}>".format(tag)
 #   hash_tag2 = "</{}>".format(tag)
 #   return hash_tag + word + hash_tag2
+
+# def make_tags(tag, word):
+#   return "<" + tag + ">" + word + "</" + tag + ">"
+
+
+# Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g. "<<word>>".
+#
+# make_out_word('<<>>', 'Yay') → '<<Yay>>'
+# make_out_word('<<>>', 'WooHoo') → '<<WooHoo>>'
+# make_out_word('[[]]', 'word') → '[[word]]'
+#
+#
+# def make_out_word(out, word):
+#   return out[:2] + word + out[2:]
+
+
+# Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
+#
+# extra_end('Hello') → 'lololo'
+# extra_end('ab') → 'ababab'
+# extra_end('Hi') → 'HiHiHi'
+#
+#
+# def extra_end(str):
+#  return str[-2:] * 3
+
+
+ 
+#  Given a string, return the string made of its first two chars, so the String "Hello" yields "He". If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "".
+
+# first_two('Hello') → 'He'
+# first_two('abcdefg') → 'ab'
+# first_two('ab') → 'ab'
+
+
+# def first_two(str):
+#   if (len > 2):
+#     return str[:2]
+#   else:
+#     return str
+
+
+# def first_two(str):
+#   if len(str) <= 2:
+#     return str
+#   return str[:2]
+  
+  
+#  Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+# first_half('WooHoo') → 'Woo'
+# first_half('HelloThere') → 'Hello'
+# first_half('abcdef') → 'abc'
+#
+# def first_half(str):
+#   return str[:len(str)/2]
+  
+  
+  
+#  Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
+
+# without_end('Hello') → 'ell'
+# without_end('java') → 'av'
+# without_end('coding') → 'odin'
+#
+# def without_end(str):
+#   return str[1:-1]
+  
+  
+ 
+  
+#  Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0).
+
+# combo_string('Hello', 'hi') → 'hiHellohi'
+# combo_string('hi', 'Hello') → 'hiHellohi'
+# combo_string('aaa', 'b') → 'baaab'
+#
+# def combo_string(a, b):
+#   if len(a) > len(b):
+#     return b + a + b
+#   return a + b + a
+
+  
+  
+ # Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+
+# non_start('Hello', 'There') → 'ellohere'
+# non_start('java', 'code') → 'avaode'
+# non_start('shotl', 'java') → 'hotlava'
+#
+# def non_start(a, b):
+#   return a[1:] + b[1:]
+
+  
+  
+#  Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end. The string length will be at least 2.
+
+# left2('Hello') → 'lloHe'
+# left2('java') → 'vaja'
+# left2('Hi') → 'Hi'
+#
+#
+# def left2(str):
+#   return str[2:] + str[:2]
