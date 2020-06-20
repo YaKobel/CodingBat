4a3f0fd57b8cfff10737ec3c9e52ec9ab54b03bf
Yaroslav Kobel <yarokobel@gmail.com>
2020-06-20 22:29:12 +0300

    add code 2

--- CodingBat/String-1.py
+++ CodingBat/String-1.py
@@ -53,7 +53,7 @@
 
 
 # Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
-#
+
 # extra_end('Hello') → 'lololo'
 # extra_end('ab') → 'ababab'
 # extra_end('Hi') → 'HiHiHi'
@@ -69,15 +69,15 @@
 # first_two('Hello') → 'He'
 # first_two('abcdefg') → 'ab'
 # first_two('ab') → 'ab'
-
-
+#
+#
 # def first_two(str):
 #   if (len > 2):
 #     return str[:2]
 #   else:
 #     return str
-
-
+#
+#
 # def first_two(str):
 #   if len(str) <= 2:
 #     return str
@@ -92,7 +92,7 @@
 #
 # def first_half(str):
 #   return str[:len(str)/2]
-  
+#
   
   
 #  Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
@@ -109,34 +109,34 @@
   
 #  Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0).
 
-# combo_string('Hello', 'hi') → 'hiHellohi'
-# combo_string('hi', 'Hello') → 'hiHellohi'
-# combo_string('aaa', 'b') → 'baaab'
-#
-# def combo_string(a, b):
-#   if len(a) > len(b):
-#     return b + a + b
-#   return a + b + a
+combo_string('Hello', 'hi') → 'hiHellohi'
+combo_string('hi', 'Hello') → 'hiHellohi'
+combo_string('aaa', 'b') → 'baaab'
+
+def combo_string(a, b):
+  if len(a) > len(b):
+    return b + a + b
+  return a + b + a
 
   
   
  # Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
 
-# non_start('Hello', 'There') → 'ellohere'
-# non_start('java', 'code') → 'avaode'
-# non_start('shotl', 'java') → 'hotlava'
-#
-# def non_start(a, b):
-#   return a[1:] + b[1:]
+non_start('Hello', 'There') → 'ellohere'
+non_start('java', 'code') → 'avaode'
+non_start('shotl', 'java') → 'hotlava'
+
+def non_start(a, b):
+  return a[1:] + b[1:]
 
   
   
 #  Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end. The string length will be at least 2.
 
-# left2('Hello') → 'lloHe'
-# left2('java') → 'vaja'
-# left2('Hi') → 'Hi'
-#
-#
-# def left2(str):
-#   return str[2:] + str[:2]
+left2('Hello') → 'lloHe'
+left2('java') → 'vaja'
+left2('Hi') → 'Hi'
+
+
+def left2(str):
+  return str[2:] + str[:2]
---
+++ CodingBat/Warmup-1.py
@@ -0,0 +1,235 @@
+
+# The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
+
+
+# sleep_in(False, False) → True
+# sleep_in(True, False) → False
+# sleep_in(False, True) → True
+#
+#
+# def sleep_in(weekday, vacation):
+#   if (not weekday) or vacation:
+#     return True
+#   else:
+#     return False
+
+	
+	
+#	We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
+
+
+# monkey_trouble(True, True) → True
+# monkey_trouble(False, False) → True
+# monkey_trouble(True, False) → False
+#
+# def monkey_trouble(a_smile, b_smile):
+#   if (a_smile and b_smile) or (not a_smile and not b_smile) :
+#     return True
+#   else:
+#     return False
+
+	
+# def monkey_trouble(a_smile, b_smile):
+#   if a_smile and b_smile:
+#     return True
+#   if not a_smile and not b_smile:
+#     return True
+#   return False
+	
+	
+# Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
+
+# sum_double(1, 2) → 3
+# sum_double(3, 2) → 5
+# sum_double(2, 2) → 8
+#
+#
+# def sum_double(a, b):
+#   if a == b:
+#     return 2*(a+b)
+#   else:
+#     return a + b
+#
+#
+# def sum_double(a, b):
+#   sum = a + b
+#
+#   if a == b:
+#     sum = sum * 2
+#   return sum
+
+
+
+# Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+
+#
+# diff21(19) → 2
+# diff21(10) → 11
+# diff21(21) → 0
+#
+#
+#
+# def diff21(n):
+#   if n > 21:
+#     return (n-21)*2
+#   else:
+#     return 21 - n
+#
+#
+# def diff21(n):
+#   if n > 21:
+#     return 2*abs(n-21)
+#   else:
+#     return abs(n-21)
+#
+#
+# def diff21(n):
+#   if n <= 21:
+#     return 21 - n
+#   else:
+#     return (n - 21) * 2
+
+
+# We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return True if we are in trouble.
+
+#
+# parrot_trouble(True, 6) → True
+# parrot_trouble(True, 7) → False
+# parrot_trouble(False, 6) → False
+#
+#
+# def parrot_trouble(talking, hour):
+#   if talking and (hour < 7 or hour > 20):
+#     return True
+#   else:
+#     return False
+#
+#
+# 	def parrot_trouble(talking, hour):
+#   return (talking and (hour < 7 or hour > 20))
+  
+  
+  
+
+# Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
+
+
+# makes10(9, 10) → True
+# makes10(9, 9) → False
+# makes10(1, 9) → True
+#
+#
+# def makes10(a, b):
+#   if (a == 10 or b == 10) or (a + b == 10):
+#     return True
+#   else:
+#     return False
+#
+#
+#   def makes10(a, b):
+#   return (a == 10 or b == 10 or a+b == 10)
+#
+  
+  
+  
+#  Given an int n, return True if it is within 10 of 100 or 200. Note: abs(num) computes the absolute value of a number.
+
+
+# near_hundred(93) → True
+# near_hundred(90) → True
+# near_hundred(89) → False
+#
+#
+# def near_hundred(n):
+#   if abs(n-100) <= 10 or abs(n-200) <= 10 :
+#     return True
+#   else:
+#     return False
+#
+#
+# def near_hundred(n):
+#   return ((abs(100 - n) <= 10) or (abs(200 - n) <= 10))
+#
+  
+  
+  # Given 2 int values, return True if one is negative and one is positive. Except if the parameter "negative" is True, then return True only if both are negative.
+
+
+# pos_neg(1, -1, False) → True
+# pos_neg(-1, 1, False) → True
+# pos_neg(-4, -5, True) → True
+#
+#
+# def pos_neg(a, b, negative):
+#   if negative:
+#     if a < 0 and b < 0:
+#       return True
+#     else:
+#       return False
+#   elif (a < 0 and b > 0) or (a > 0 and b < 0):
+#     return True
+#   return False
+#
+#
+# def pos_neg(a, b, negative):
+#   if negative:
+#     return (a < 0 and b < 0)
+#   else:
+#     return ((a < 0 and b > 0) or (a > 0 and b < 0))
+
+
+
+# Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged.
+#
+#
+# not_string('candy') → 'not candy'
+# not_string('x') → 'not x'
+# not_string('not bad') → 'not bad'
+
+# def not_string(str):
+#   if str.startswith("not"):
+#     return str
+#   return "not " + str
+
+# def not_string(str):
+#   if len(str) >= 3 and str[:3] == "not":
+#     return str
+#   return "not " + str
+
+
+
+# Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..len(str)-1 inclusive).
+#
+#
+# missing_char('kitten', 1) → 'ktten'
+# missing_char('kitten', 0) → 'itten'
+# missing_char('kitten', 4) → 'kittn'
+
+# def missing_char(str, n):
+#   return str[:n] + str[n+1:]
+
+# def missing_char(str, n):
+#   front = str[:n]   # up to but not including n
+#   back = str[n+1:]  # n+1 through end of string
+#   return front + back
+
+
+
+# Given a string, return a new string where the first and last chars have been exchanged.
+#
+#
+# front_back('code') → 'eodc'
+# front_back('a') → 'a'
+# front_back('ab') → 'ba'
+
+
+# def front_back(str):
+#   if len(str) <= 1:
+#     return str
+#
+#   mid = str[1:len(str) - 1]
+#   return str[len(str) - 1] + mid + str[0]
+
+
+
