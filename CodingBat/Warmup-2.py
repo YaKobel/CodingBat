4265a670e91e116c11a3a9bbe9d54d76b38638e8
Yaroslav Kobel <yarokobel@gmail.com>
2020-06-23 23:54:05 +0300

    add code

---
+++ CodingBat/Warmup-2.py
@@ -0,0 +1,80 @@
+Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+string_times('Hi', 2) → 'HiHi'
+string_times('Hi', 3) → 'HiHiHi'
+string_times('Hi', 1) → 'Hi'
+
+
+def string_times(str, n):
+  return str * n
+
+
+def string_times(str, n):
+  result = ""
+  for i in range(n):  # range(n) is [0, 1, 2, .... n-1]
+    result = result + str  # could use += here
+  return result
+
+
+Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+
+front_times('Chocolate', 2) → 'ChoCho'
+front_times('Chocolate', 3) → 'ChoChoCho'
+front_times('Abc', 3) → 'AbcAbcAbc'
+
+
+def front_times(str, n):
+  return n*(str[:3])
+
+def front_times(str, n):
+  return str[0:3] * n
+
+def front_times(str, n):
+  front_len = 3
+  if front_len > len(str):
+    front_len = len(str)
+  front = str[:front_len]
+  
+  result = ""
+  for i in range(n):
+    result = result + front
+  return result
+  
+  
+
+Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+
+string_bits('Hello') → 'Hlo'
+string_bits('Hi') → 'H'
+string_bits('Heeololeo') → 'Hello'
+
+
+def string_bits(str):
+  return str[::2] 
+
+
+def string_bits(str):
+  result = ""
+  # Many ways to do this. This uses the standard loop of i on every char,
+  # and inside the loop skips the odd index values.
+  for i in range(len(str)):
+    if i % 2 == 0:
+      result = result + str[i]
+  return result
+
+def string_bits(str):
+  result = ""
+  for x in xrange(0,len(str),2):
+    result += str[x]
+  return result
+
+  
+
+Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+string_splosion('Code') → 'CCoCodCode'
+string_splosion('abc') → 'aababc'
+string_splosion('ab') → 'aab'
+
+
+
