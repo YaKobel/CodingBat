1c6e127d6fbac95c9bee5fbc5a530deeb6b45798
Yaroslav Kobel <yarokobel@gmail.com>
2020-07-02 23:20:04 +0300

    add code

--- CodingBat/Logic-1.py
+++ CodingBat/Logic-1.py
@@ -5,19 +5,19 @@ Logic-1 > cigar_party
 cigar_party(30, False) → False
 cigar_party(50, False) → True
 cigar_party(70, True) → True
-
+'''
 def cigar_party(cigars, is_weekend):
   if is_weekend == False:
     return cigars >= 40 and cigars <=60
     
   return cigars >= 40
-  
-  
+  '''
+  '''
 def cigar_party(cigars, is_weekend):
   if is_weekend == False:
     return  40 <= cigars <=60
   return cigars >= 40
-  
+ ''' 
   
   
   
@@ -26,15 +26,15 @@ def cigar_party(cigars, is_weekend):
 date_fashion(5, 10) → 2
 date_fashion(5, 2) → 0
 date_fashion(5, 5) → 1
-  
+ ''' 
 def date_fashion(you, date):
   if you <= 2 or date <=2:
     return 0
   if you >= 8 or date >= 8:
     return 2
   return 1
-  
-  
+ ''' 
+'''  
 def date_fashion(you, date):
   if you <= 2 or date <=2:
     return 0
@@ -42,8 +42,8 @@ def date_fashion(you, date):
     return 2
   else:
     return 1
-  
-  
+ ''' 
+'''  
   
 def date_fashion(you, date):
   if (you >= 8 or date >= 8) and (you > 2 and date > 2):
@@ -51,7 +51,7 @@ def date_fashion(you, date):
   if you <= 2 or date <=2:
     return 0
   return 1
-  
+ ''' 
   
 # The squirrels in Palo Alto spend most of the day playing. In particular, they play if the temperature is between 60 and 90 (inclusive). Unless it is summer, then the upper limit is 100 instead of 90. Given an int temperature and a boolean is_summer, return True if the squirrels play and False otherwise.
 
@@ -59,19 +59,19 @@ squirrel_play(70, False) → True
 squirrel_play(95, False) → False
 squirrel_play(95, True) → True
 
-
+'''
 def squirrel_play(temp, is_summer):
   if is_summer == True:
     return temp >= 60 and temp <= 100
   return temp >= 60 and temp <= 90
+'''  
   
-  
-  
+'''  
 def squirrel_play(temp, is_summer):
   if is_summer == True:
     return 60 <=  temp <= 100
   return 60 <= temp <= 90
-  
+ ''' 
   
   
 #  You are driving a little too fast, and a police officer stops you. Write code to compute the result, encoded as an int value: 0=no ticket, 1=small ticket, 2=big ticket. If speed is 60 or less, the result is 0. If speed is between 61 and 80 inclusive, the result is 1. If speed is 81 or more, the result is 2. Unless it is your birthday -- on that day, your speed can be 5 higher in all cases.
@@ -80,7 +80,7 @@ caught_speeding(60, False) → 0
 caught_speeding(65, False) → 1
 caught_speeding(65, True) → 0
 
-
+'''
 def caught_speeding(speed, is_birthday):
   speed_adj = 0
   
@@ -92,11 +92,125 @@ def caught_speeding(speed, is_birthday):
   if speed < 81 + speed_adj:
     return 1
   return 2
+'''
 
 
+# Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
 
+sorta_sum(3, 4) → 7
+sorta_sum(9, 4) → 20
+sorta_sum(10, 11) → 21
 
-
-
-
+'''
+def sorta_sum(a, b):
+  sum = a+b
   
+  if 10 <= sum <= 19: # if sum >= 10 and <= 19
+    return 20
+  
+  return a+b
+  '''
+  
+# Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean indicating if we are on vacation, return a string of the form "7:00" indicating when the alarm clock should ring. Weekdays, the alarm should be "7:00" and on the weekend it should be "10:00". Unless we are on vacation -- then on weekdays it should be "10:00" and weekends it should be "off".
+
+alarm_clock(1, False) → '7:00'
+alarm_clock(5, False) → '7:00'
+alarm_clock(0, False) → '10:00'
+
+'''
+def alarm_clock(day, vacation):
+  
+  if day == 0 or day == 6:
+    if vacation:
+      return "off"
+    return "10:00"
+    
+  if vacation:
+    return "10:00"
+  return "7:00"
+'''
+  
+  
+  '''
+def alarm_clock(day, vacation):
+  weekend = "off"
+  weekday = "10:00"
+  
+  if (vacation == False):
+    weekend = "10:00"
+    weekday = "7:00"
+    
+  if day % 6 == 0:
+    return weekend
+  return weekday
+'''
+
+'''
+def alarm_clock(day, vacation):
+
+  normalDay = ["10:00","7:00","7:00","7:00","7:00","7:00","10:00"]
+  vacationDay = ["off","10:00","10:00","10:00","10:00","10:00","off"]
+  
+  if (vacation):
+    return vacationDay[day]
+  return normalDay[day]
+'''
+
+
+The number 6 is a truly great number. Given two int values, a and b, return True if either one is 6. Or if their sum or difference is 6. Note: the function abs(num) computes the absolute value of a number.
+
+love6(6, 4) → True
+love6(4, 5) → False
+love6(1, 5) → True
+
+'''
+def love6(a, b):
+  return (a == 6 or b == 6) or (a + b == 6 or b - a == 6 or a - b == 6)
+'''
+
+def love6(a, b):
+  return (a == 6 or b == 6) or (a + b == 6) or abs(a - b) == 6
+
+
+
+
+Given a number n, return True if n is in the range 1..10, inclusive. Unless outside_mode is True, in which case return True if the number is less or equal to 1, or greater or equal to 10.
+
+in1to10(5, False) → True
+in1to10(11, False) → False
+in1to10(11, True) → True
+
+'''
+def in1to10(n, outside_mode):
+ 
+  if outside_mode:
+    return n <= 1 or n >= 10
+    
+  return 1 <= n <= 10
+'''
+
+
+Given a non-negative number "num", return True if num is within 2 of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2. See also: Introduction to Mod
+
+near_ten(12) → True
+near_ten(17) → False
+near_ten(19) → True
+
+
+'''
+def near_ten(num):
+  if num%10 == 8 or num%10 == 9 or num%10 == 0 or num%10 == 1 or num%10 == 2:
+    return True
+  return False
+'''
+'''
+def near_ten(num):
+  return num%10 == 8 or num%10 == 9 or num%10 == 0 or num%10 == 1 or num%10 == 2
+'''
+
+def near_ten(num):
+  return 0 <= (num%10 + 2) % 10 <= 4
+
+
+
+
