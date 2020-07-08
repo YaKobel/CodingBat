daae1fdbe7e1c4ee0d307d34f8497650956046cf
Yaroslav Kobel <yarokobel@gmail.com>
2020-07-08 07:01:33 +0300

    add code

--- CodingBat/String-2.py
+++ CodingBat/String-2.py
@@ -2,9 +2,9 @@ String-2 > double_char
 
 # Given a string, return a string where for every char in the original, there are two chars.
 
-double_char('The') → 'TThhee'
-double_char('AAbb') → 'AAAAbbbb'
-double_char('Hi-There') → 'HHii--TThheerree'
+# double_char('The') → 'TThhee'
+# double_char('AAbb') → 'AAAAbbbb'
+# double_char('Hi-There') → 'HHii--TThheerree'
 
 '''
 def double_char(str):
@@ -22,13 +22,16 @@ def double_char(str):
     result = result + str[i] + str[i]
   return result
 
-  
+
+def double_char(str):
+  return str[0:1]*2 + str[1:2]*2 + str[2:3]*2 + str[3:4]*2 + str[4:5]*2 + str[5:6]*2 + str[6:7]*2 + str[7:8]*2 #bad long way
+   
 
 # Return the number of times that the string "hi" appears anywhere in the given string.
 
-count_hi('abc hi ho') → 1
-count_hi('ABChi hi') → 2
-count_hi('hihi') → 2
+# count_hi('abc hi ho') → 1
+# count_hi('ABChi hi') → 2
+# count_hi('hihi') → 2
 
 '''
 def count_hi(str):
@@ -119,3 +122,64 @@ def cat_dog(str):
   '''
   
   
+# Return the number of times that the string "code" appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+
+# count_code('aaacodebbb') → 1
+# count_code('codexxcode') → 2
+# count_code('cozexxcope') → 2
+#
+
+def count_code(str):
+  count = 0
+  
+  for i in range(0, len(str) - 3, 1):
+    
+    if (str[i] == 'c' and str [i+1] == 'o' and str [i+3] == 'e'):
+      count = count + 1
+      
+  return count
+
+# Given two strings, return True if either of the strings appears at the very end of the other string, ignoring upper/lower case differences (in other words, the computation should not be "case sensitive"). Note: s.lower() returns the lowercase version of a string.
+
+# end_other('Hiabc', 'abc') → True
+# end_other('AbC', 'HiaBc') → True
+# end_other('abc', 'abXabc') → True
+
+
+def end_other(a, b):
+  a = a.lower()
+  b = b.lower()
+  
+  lstr = a
+  sstr = b
+  
+  if len(b) > len(a):
+    lstr = b
+    sstr = a
+    
+  if sstr == lstr[len(lstr) - len(sstr): len(lstr)]:
+    return True
+  return False
+
+# Return True if the given string contains an appearance of "xyz" where the xyz is not directly preceeded by a period (.). So "xxyz" counts but "x.xyz" does not.
+
+# xyz_there('abcxyz') → True
+# xyz_there('abc.xyz') → False
+# xyz_there('xyz.abc') → True
+
+
+def xyz_there(str):
+  return str.count("xyz") - str.count(".xyz") > 0
+
+def xyz_there(str):
+  return str.replace(".xyz", "").find("xyz") >= 0
+
+
+def xyz_there(str):
+  if len(str) >= 3 and str[0:3] == "xyz":
+    return True
+
+  for i in range(1, len(str) - 2, 1):
+    if str[i: i + 3] == "xyz" and str[i - 1] != ".":
+      return True
+  return False
